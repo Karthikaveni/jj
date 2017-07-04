4a19acf0e5fe421689af7d177a23ceaaca8044f8
Karthikaveni <karthikaveni18@gmail.com>
2017-07-04 17:07:08 +0530

    Create palindromeDemo.java

---
+++ palindromeDemo.java
@@ -0,0 +1,26 @@
+# jj
+import java.io.*;
+import java.util.*;
+public class palindromeDemo{
+  
+  public static void main(String args[])throws IOException
+  {
+    
+ BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ String str;
+ str=br.readLine();
+ String revstring="";
+ for(int i=str.length()-1;i>=0;--i)
+ {
+  revstring +=str.charAt(i);
+  }
+  System.out.println(revstring);
+  if(revstring.equalsIgnoreCase(str)){
+  System.out.println("The String is palindrome");
+  }
+  else{
+    System.out.println("not palindrome");
+    }
+    }
+    }
+   
